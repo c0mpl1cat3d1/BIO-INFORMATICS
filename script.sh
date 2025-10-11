c38b87d1f7c14fef36c99b57f6ce61bb1f257c30
mou288 <moulighosh2882003@gmail.com>
2025-10-11 17:25:31 +0530

    delete file

--- script.sh
+++
@@ -1,9 +0,0 @@
-# Create the main project folder and navigate into it
-mkdir Virus_Classifier
-cd Virus_Classifier
-
-# Create all sub-directories in one command
-mkdir -p data/raw data/processed src models results/plots results/reports
-
-# Create the empty files
-touch README.md requirements.txt .gitignore src/01_download_data.py src/02_preprocess_data.py src/03_train_model.py src/04_evaluate_model.py src/utils.py
